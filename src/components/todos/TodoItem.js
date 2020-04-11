8a94cce289501983378179e1dd97f77dd8c8fa9b
hamid <hamid331994@gmail.com>
2020-04-11 19:31:34 +0430

    mark todo Done

--- src/components/todos/TodoItem.js
+++ src/components/todos/TodoItem.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { TodoContext } from '../../context/todo/TodoState';
 
 const TodoItem = ({ todo }) => {
-  const { deleteTodo, setCurrent } = useContext(TodoContext);
+  const { deleteTodo, setCurrent, updateTodo } = useContext(TodoContext);
 
   const onDelete = () => {
     deleteTodo(todo._id);
@@ -12,14 +12,26 @@ const TodoItem = ({ todo }) => {
     setCurrent(todo);
   };
 
+  const toggleDone = () => {
+    updateTodo(
+      {
+        done: !todo.done,
+      },
+      todo._id
+    );
+  };
+
   return (
-    <div className="todo-item">
+    <div className={`todo-item ${todo.done && 'done'}`}>
       <span>{todo.text}</span>
       <span className="delete" onClick={onDelete}>
         X
       </span>
       <span className="edit" onClick={onEdit}>
         E
+      </span>
+      <span className="edit" onClick={toggleDone}>
+        D
       </span>
     </div>
   );
